c79572476c66c80567088c6090289600aeca40ec
aballo <aballo@pixup.cat>
2023-06-05 13:32:43 +0200

    hexagonal arquitecture app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 const app = express();
 app.use(cors());
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('listening on port:' + process.env.PORT);
 });
